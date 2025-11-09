36fb6c5a78bb3dfd47a8f5c4397f449eb501eab1
ISMAIL REHMAN <75415362+Ismailrehman9@users.noreply.github.com>
2025-11-09 17:49:27 +0500

    Update google.php

--- config/google.php
+++ config/google.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'sheet_id' => '1dARfot-bjfE3PNHMfLe4HmEeUuusvegeR9pVmodQ4Yg',
-    'credentials' => '/Users/cellulargeeks/Documents/Github/qr-code/storage/app/google-service-account.json',
+    'sheet_id' => 'YOUR_SHEET_KEY',
+    'credentials' => 'YOU_FILE_PATH/google-service-account.json',
 ];
